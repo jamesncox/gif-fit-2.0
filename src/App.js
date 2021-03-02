7c92276534f201b958d54ff37ccf218173852c48
jamesncox <jamesncox@yahoo.com>
2021-03-02 14:27:39 -0600

    Stub out TopNav.js and create TopNav.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './stylesheets/App.css'
+import './stylesheets/TopNav.css'
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'
 
+import TopNav from './components/layout/TopNav'
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <TopNav />
         <h1>GIF FIT</h1>
       </div>
     </Router>
