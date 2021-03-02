a84a146b2b4ef5102b6d7102239e152a395d42fe
jamesncox <jamesncox@yahoo.com>
2021-03-02 15:17:14 -0600

    Stub out TopNav.js and TopNav.css

--- src/components/layout/TopNav.js
+++ src/components/layout/TopNav.js
@@ -4,10 +4,43 @@ function TopNav(props) {
 
     return (
         <div class="navbar">
-            <a href="#home">Home</a>
-            <a href="#news">News</a>
+            <h1 className="title">GIF FIT</h1>
+
             <div class="dropdown">
-                <button class="dropbtn">Dropdown
+                <button class="dropbtn">No. Exercises
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button class="dropbtn">Exercise Time
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button class="dropbtn">Rest Time
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button class="dropbtn">No. Rounds
                     <i class="fa fa-caret-down"></i>
                 </button>
                 <div class="dropdown-content">
