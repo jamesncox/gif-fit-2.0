940c51cdc7b4aa8a8e1424376390188b02996771
jamesncox <jamesncox@yahoo.com>
2021-03-06 16:33:47 -0600

    Add react-transition-group

--- src/hooks/Modal.js
+++ src/hooks/Modal.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import './styles/transitions.css'
 
-const Modal = props => {
-    return (
-        <div>
-            {props.children}
-        </div>
-    );
-};
+import React, { useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import { createPortal } from 'react-dom'
 
-export default Modal;
+const Modal = ({ children, activator }) => {
+  const [show, setShow] = useState(false)
+
+  const content = (
+    <div className="overlay">
+      <div className="modal">
+        <button
+          className="modal-close"
+          type="button"
+          onClick={() => setShow(false)}
+        >
+          X
+        </button>
+        <div className="modal-body">{children}</div>
+      </div>
+    </div>
+  )
+
+  return (
+    <>
+      {activator({ setShow })}
+      {createPortal(
+        <CSSTransition
+          in={show}
+          timeout={120}
+          classNames="modal-transition"
+          unmountOnExit
+        >
+          {() => <div>{content}</div>}
+        </CSSTransition>,
+        document.body
+      )}
+    </>
+  )
+}
+
+export default Modal
