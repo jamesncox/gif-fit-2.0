ac6556974ef48988b59c0b8941914437515f7587
jamesncox <jamesncox@yahoo.com>
2021-03-07 11:22:45 -0600

    Import Workout.js to App.js and create Route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
 import TopNav from './components/layout/TopNav'
 import SideNav from './components/layout/SideNav'
 import Home from './components/layout/Home'
+import Workout from './components/layout/Workout'
 
 function App() {
   return (
@@ -24,6 +25,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/workout">
+            <Workout />
           </Route>
         </Switch>
       </div>
--- src/components/layout/Workout.js
+++ src/components/layout/Workout.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-function Workout(props) {
+function Workout(props) { 
+
+    return (
+        null
+    )
 
 }
 
